e8c54f7edde2f7c257fc2a016261411869218e1e
Rostyslav Diachok <infernalmaster1@gmail.com>
2015-01-09 02:32:43 +0200

    ups packages and fix source maps for sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var handleErrors = require('../util/handleErrors');
 var plumber = require('gulp-plumber');
+var sourcemaps   = require('gulp-sourcemaps');
 // var autoprefixer    = require('gulp-autoprefixer');
 
 // Compile and Automatically Prefix Stylesheets
@@ -11,7 +12,14 @@ gulp.task('sass', function () {
   // For best performance, don't add Sass partials to `gulp.src`
   return gulp.src(['src/sass/*.{scss, sass}'])
     .pipe(plumber({errorHandler: handleErrors}))
-    .pipe(sass({sourceComments: 'map'}))
+    .pipe(sourcemaps.init({}))
+    .pipe(sass({
+      // Required if you want to use SASS syntax
+      // See https://github.com/dlmanning/gulp-sass/issues/81
+      sourceComments: 'map'
+      // imagePath: '/images' // Used by the image-url helper
+    }))
+    .pipe(sourcemaps.write())
     // .pipe(autoprefixer(AUTOPREFIXER_BROWSERS, { map: true }))
     .pipe(gulp.dest(dest));
 });
